3051e00f6f1c7f93f3a43341d837b997adb8a1c7
IsaelSousa <toxicrootbeta@gmail.com>
2021-02-13 17:05:18 -0300

    commit

--- index.js
+++ index.js
@@ -8,6 +8,31 @@ $(document).ready(function () {
   });
 
   
+  $("#content").click(function(){
+
+    $(this).select();
+
+    document.execCommand('copy');
+
+  })
+
   function btnText(texto) {
     document.querySelector('#texto1').innerText = texto;
+  }
+
+  function removeSpace() {
+    var value = document.querySelector('#send-rm').value
+
+    var replace = value.replaceAll(" ", "").replaceAll(".", "")
+    document.querySelector('#content').value = replace
+
+    var contentvalue = document.querySelector('#content').value
+    document.querySelector('#length').value = contentvalue.length
+  }
+
+  function removeSpaceClear() {
+    document.querySelector('#send-rm').value = ""
+    document.querySelector('#content').value = ""
+    document.querySelector('#length').value = "0"
+
   }
