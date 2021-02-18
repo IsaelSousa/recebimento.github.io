e91c209b408c0ea5afc3ccaee6281d8250374367
IsaelSousa <toxicrootbeta@gmail.com>
2021-02-17 21:03:01 -0300

    commit

--- index.js
+++ index.js
@@ -20,7 +20,12 @@ $(document).ready(function () {
     document.querySelector('#texto1').innerText = texto;
   }
 
+
+
+
   function removeSpace() {
+    var i = 16;
+
     var value = document.querySelector('#send-rm').value
 
     var replace = value.replaceAll(" ", "").replaceAll(".", "")
@@ -28,11 +33,26 @@ $(document).ready(function () {
 
     var contentvalue = document.querySelector('#content').value
     document.querySelector('#length').value = contentvalue.length
+
+      
+
+      var timer = setInterval(function(func) {
+      i--;
+      document.getElementById("relogio").value = i;
+      
+        if(i <= 0){
+          clearInterval(timer)
+        document.querySelector('#send-rm').value = ""
+        document.querySelector('#content').value = ""
+        document.querySelector('#length').value = "0"
+        }
+
+    }, 1000);    
+
+
+
+
   }
 
-  function removeSpaceClear() {
-    document.querySelector('#send-rm').value = ""
-    document.querySelector('#content').value = ""
-    document.querySelector('#length').value = "0"
 
-  }
+
